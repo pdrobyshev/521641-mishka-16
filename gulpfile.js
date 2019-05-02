54a083b05f8aabc29a29dbf330eef5e11336fc00
pdrobyshev <pdrobyshev@wooppay.com>
2019-05-02 09:40:15 +0600

    добавил js либы через npm, добавил для них таск и подключил полифилл для свг на страницах

--- gulpfile.js
+++ gulpfile.js
@@ -29,20 +29,28 @@ gulp.task("copy", function () {
   ], {
     base: "source"
   })
-    .pipe(gulp.dest("build"))
+      .pipe(gulp.dest("build"))
+});
+
+gulp.task("copy-js-libs", function () {
+  return gulp.src([
+    "node_modules/picturefill/dist/picturefill.min.js",
+    "node_modules/svg4everybody/dist/svg4everybody.min.js"
+  ])
+      .pipe(gulp.dest("build/js"))
 });
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+      .pipe(plumber())
+      .pipe(sourcemap.init())
+      .pipe(sass())
+      .pipe(postcss([ autoprefixer() ]))
+      .pipe(gulp.dest("build/css"))
+      .pipe(csso())
+      .pipe(rename("style.min.css"))
+      .pipe(sourcemap.write("."))
+      .pipe(gulp.dest("build/css"))
 });
 
 gulp.task("refresh", function (done) {
@@ -66,19 +74,19 @@ gulp.task("server", function () {
 
 gulp.task("images", function () {
   return gulp.src("build/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("build/img"))
+      .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.svgo()
+      ]))
+      .pipe(gulp.dest("build/img"))
 });
 
 gulp.task("webp", function () {
   return gulp.src("build/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"))
-    .pipe(gulp.dest("build/img"))
+      .pipe(webp({quality: 90}))
+      .pipe(gulp.dest("source/img"))
+      .pipe(gulp.dest("build/img"))
 });
 
 gulp.task("sprite", function () {
@@ -87,37 +95,39 @@ gulp.task("sprite", function () {
     "build/img/logo-footer.svg",
     "build/img/logo-htmlacademy.svg"
   ])
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"))
+      .pipe(svgstore({
+        inlineSvg: true
+      }))
+      .pipe(rename("sprite.svg"))
+      .pipe(gulp.dest("build/img"))
 });
 
 gulp.task("html", function () {
   return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"))
+      .pipe(posthtml([
+        include()
+      ]))
+      .pipe(gulp.dest("build"))
 });
 
 gulp.task("build", gulp.series(
-  "clean",
-  "copy",
-  "css",
-  "sprite",
-  "html"
+    "clean",
+    "copy",
+    "copy-js-libs",
+    "css",
+    "sprite",
+    "html"
 ));
 
 gulp.task("prod-build", gulp.series(
-  "clean",
-  "copy",
-  "css",
-  "images",
-  "webp",
-  "sprite",
-  "html"
+    "clean",
+    "copy",
+    "copy-js-libs",
+    "css",
+    "images",
+    "webp",
+    "sprite",
+    "html"
 ));
 
 gulp.task("start", gulp.series("build", "server"));
